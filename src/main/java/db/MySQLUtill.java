be42b49796d5257f73530c01d823e90fc8dac8c1
‘asel98x’ <vinjith98@gmail.com>
2022-08-27 00:54:51 +0530

    sone updates

--- src/main/java/db/MySQLUtill.java
+++ src/main/java/db/MySQLUtill.java
@@ -23,15 +23,15 @@ import model.vehicleCategory;
  * @author asel
  */
 public class MySQLUtill implements DBUtill{
-//    Connection con = null;
-//    Statement pst = null;
-//    ResultSet rs = null;
-    static final String DB_URL = "jdbc:mysql://localhost:3306/gocheetah?zeroDateTimeBehavior=CONVERT_TO_NULL";
-    static final String USER = "root";
-    static final String PASS = "";
+    Connection con = null;
+    Statement pst = null;
+    ResultSet rs = null;
+//    static final String DB_URL = "jdbc:mysql://localhost:3306/gocheetah?zeroDateTimeBehavior=CONVERT_TO_NULL";
+//    static final String USER = "root";
+//    static final String PASS = "";
     
     public MySQLUtill() {
-        //con = dbConnection.connect(); 
+        con = dbConnection.connect(); 
         
         
     }
@@ -40,11 +40,11 @@ public class MySQLUtill implements DBUtill{
     public boolean customerSignup(customer cs) {
         int rowsAffected = 0;
         try {
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement statement = conn.createStatement();
+//            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+//            Statement statement = conn.createStatement();
 //            String sql = "INSERT INTO `customer` VALUES ('" + cs.getName()+ "','" + cs.getAddress()+ "', " + cs.getMobile()+ ", '" + cs.getNic()+ "', '" + cs.getEmail()+ "')";
-//            pst = con.createStatement();
-            rowsAffected = statement.executeUpdate("INSERT INTO `customer` (Customer_name, Customer_address, Customer_mobile, Customer_nic, Customer_email, Customer_password) VALUES ('" + cs.getName()+ "','" + cs.getAddress()+ "', " + cs.getMobile()+ ", '" + cs.getNic()+ "', '" + cs.getEmail()+ "', '" + cs.getPassword()+ "')");
+            pst = con.createStatement();
+            rowsAffected = pst.executeUpdate("INSERT INTO `customer` (Customer_name, Customer_address, Customer_mobile, Customer_nic, Customer_email, Customer_password) VALUES ('" + cs.getName()+ "','" + cs.getAddress()+ "', " + cs.getMobile()+ ", '" + cs.getNic()+ "', '" + cs.getEmail()+ "', '" + cs.getPassword()+ "')");
             //rowsAffected = pst.executeUpdate("INSERT INTO `customer` (Customer_name, Customer_address, Customer_mobile, Customer_nic, Customer_email) VALUES ('asel','address', 0123456789, '0123456nic', 'gmail.com')");
         } catch(Exception e) {
             System.out.println(e);
